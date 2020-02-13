3170499dc0bc4e0ba26482e0f02e08284d5a4c19
Peter St. John <peterc.stjohn@gmail.com>
2020-02-13 12:36:11 -0700

    adding to package_data

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='EnsembleModelingLinLog',
+    name='emll',
     version='0.1.1',
     packages=find_packages(),
+    package_data={'emll': ['emll/test_models/*.json',
+                           'emll/test_models/*.p',
+                           'emll/test_models/*.xml']},
     license="LGPL/GPL v2+",
     long_description=open('README.md').read(),
 )
