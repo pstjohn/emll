eddc56c7189cfb62cb03ce463f19fd09e3223657
Peter St. John <peterc.stjohn@gmail.com>
2020-02-13 12:50:55 -0700

    using include_package_data

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ setup(
     name='emll',
     version='0.1.1',
     packages=find_packages(),
-    package_data={'emll': ['emll/test_models/*.json',
-                           'emll/test_models/*.p',
-                           'emll/test_models/*.xml']},
+    include_package_data=True,
     license="LGPL/GPL v2+",
     long_description=open('README.md').read(),
 )
