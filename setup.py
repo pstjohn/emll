82d6e896c8295063fc6c060a357704b87307e024
Peter St. John <peterc.stjohn@gmail.com>
2020-02-13 12:24:33 -0700

    adding models to manifest

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='EnsembleModelingLinLog',
-    version='0.1',
+    version='0.1.1',
     packages=find_packages(),
     license="LGPL/GPL v2+",
     long_description=open('README.md').read(),
